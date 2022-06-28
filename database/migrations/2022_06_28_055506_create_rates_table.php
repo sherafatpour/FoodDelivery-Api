2578a3eda2247741b10fa2f0986dcb55931486ba
sherafatpour <reza.sharafatpour@gmail.com>
2022-06-28 10:32:44 +0430

    create  rate section (policy - migration - model - controller - seeder)

---
+++ database/migrations/2022_06_28_055506_create_rates_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rates', function (Blueprint $table) {
+            $table->id();
+            $table->double('rate')->default(0.0);
+            $table->morphs('rateable');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rates');
+    }
+}
